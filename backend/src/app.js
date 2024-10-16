079337fbec9f382bb5900ad5e840f8a780057786
Tanmay <tanmay.json@gmail.com>
2024-10-17 02:03:43 +0530

    hogggaaa

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ app.use(cors()); // Allow CORS for all origins or specify a domain in production
 app.use(express.json());
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(express.static(path.join(__dirname, '../../frontend/build')));
 
 // Define your API routes
 app.use('/admin/alerts', AlertsRouter);
@@ -21,7 +21,7 @@ app.use('/', temp);
 
 // Catch all other requests and return the React app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+  res.sendFile(path.join(__dirname, '../../frontend/build/index.html'));
 });
 
 module.exports = app;

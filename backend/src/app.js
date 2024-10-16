d469d703d51da2cf53f4360d791018c0d030d589
Tanmay <tanmay.json@gmail.com>
2024-10-17 01:49:06 +0530

    backendA

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+const path = require('path');
 const app = express();
 const AlertsRouter = require('./routes/alerts/alerts.routes');
 const ComplainsRouter = require('./routes/complains/complains.routes')
 const temp = require('./routes/temperature/temperature')
 
 // Enable CORS for port 3000
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.use(cors({
-  origin: 'http://localhost:3000' // Adjust this to match your frontend URL
+  origin: 'http://localhost:3000'
 }));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html')); 
 
 app.use(express.json());
 app.use('/admin/alerts', AlertsRouter);
